4dc3fa284b41bcfa3478a8b0037ec07bed5cd3a0
matheus-bmsilva <matheus.barros.msilva@gmail.com>
2025-11-20 15:53:57 -0300

    Create Cores.py
    Arquivo com configuração de formatação de texto em diferentes cores e formatação, facilitando a formatação de texto no código principal.

---
+++ Cores.py
@@ -0,0 +1,26 @@
+# Cores do Padrão ANSI
+preto = "\033[0;30m"
+vermelho = "\033[0;31m"
+verde = "\033[0;32m"
+amarelo = "\033[0;33m"
+azul = "\033[0;34m"
+magenta = "\033[0;35m"
+ciano = "\033[0;36m"
+branco = "\033[0;37m"
+vermelho_claro = "\033[38;5;160m"
+
+# Cores em negrito
+preto_negrito = "\033[1;30m"
+vermelho_negrito = "\033[1;31m"
+verde_negrito = "\033[1;32m"
+amarelo_negrito = "\033[1;33m"
+azul_negrito = "\033[1;34m"
+magenta_negrito = "\033[1;35m"
+ciano_negrito = "\033[1;36m"
+branco_negrito = "\033[1;37m"
+
+# Resetar cor
+resetar = "\033[0m"
+
+# Italico
+ITALIC = "\033[3m"
